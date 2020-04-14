1dee3f1a42a93d3bf90a7539e3a27d308ae3dcaa
emile <bfdlmdr@gmail.com>
2020-04-14 07:09:23 +0200

    I must enjoy torturing myself

---
+++ build/precache-manifest.71ce487dbb94087aa1ad011da096d6d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc467970e9c90573fabe659107addccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3858160545d501136b3",
+    "url": "/static/css/main.7f5dd20c.chunk.css"
+  },
+  {
+    "revision": "337d9ae84843bb5753c9",
+    "url": "/static/js/2.ff549f32.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/static/js/2.ff549f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3858160545d501136b3",
+    "url": "/static/js/main.63396f97.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "933db819beea3a59caaebb94439b7df9",
+    "url": "/static/media/07180db8.933db819.svg"
+  }
+]);
